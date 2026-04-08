2a72bb2980ddd5cac7f0ca9dcece776b8d85606b
giosteffe <giosteffenino@gmail.com>
2026-04-08 19:00:05 +0200

    Lezione 23/02

--- main.py
+++ main.py
@@ -56,7 +56,7 @@ class Cliente:
         if categoria not in categorie_valide:
             raise ValueError("Categoria non valida, Scegliere tra Gold, Silver e Bronze")
         else:
-            self._category = categoria
+            self._category = categoria #CIAO
 
     def descrizione(self):
         return f"Cliente {self.name} ({self.category}) - {self.mail}"
